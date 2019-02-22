e815001aea557d791bbc8e1f0a2ea25b03984c83
Juan Reyes <seyerman@gmail.com>
2019-02-21 23:37:45 -0500

    QuotaEnrollExceedException class exception was fully coded

--- src/customExceptions/QuotaEnrollExceedException.java
+++ src/customExceptions/QuotaEnrollExceedException.java
@@ -2,5 +2,14 @@ package customExceptions;
 
 @SuppressWarnings("serial")
 public class QuotaEnrollExceedException extends Exception{
-
+	private int quota;
+	
+	private QuotaEnrollExceedException(int q) {
+		super("Quota exceeded. The enrollment is full. Quota is "+q);
+		quota = q;
+	}
+	
+	public int getQuota() {
+		return quota;
+	}
 }
